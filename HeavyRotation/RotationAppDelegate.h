2a7a9b40ccae15f3083defbe6d8e24d6509cbf83
Richard Millet <remillet@yahoo.com>
2012-06-19 20:14:56 -0700

    Initial Commit

---
+++ HeavyRotation/RotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Sara Duckler on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
