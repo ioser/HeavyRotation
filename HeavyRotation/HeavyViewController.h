ed986aa8e443d9152f911774b0f57d98e9e5ba5e
Richard Millet <remillet@yahoo.com>
2012-06-20 23:43:10 -0700

    Autorotating Izzy.

---
+++ HeavyRotation/HeavyViewController.h
@@ -0,0 +1,13 @@
+//
+//  HeavyViewController.h
+//  HeavyRotation
+//
+//  Created by Sara Duckler on 6/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeavyViewController : UIViewController
+
+@end
